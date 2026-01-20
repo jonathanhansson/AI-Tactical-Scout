9979d7f8b5c8cb3eceab62824603f17f543502aa
jonathanhansson <jonathanhanssondg@gmail.com>
2026-01-20 18:04:07 +0100

    FEATURE: player_generator.py now imports from agents.py

--- src/backend/player_generator.py
+++ src/backend/player_generator.py
@@ -1,32 +1,13 @@
 from data_models import Player
+from agents import generator_agent, writer_agent
 from pydantic_ai import Agent
 from dotenv import load_dotenv
 import os
 import asyncio
 import random
 
+
 load_dotenv()
-
-generator_agent = Agent(
-    model="google-gla:gemini-2.5-flash",
-    output_type=Player, # using 'output_type' instead of 'result_type' to comply with latest documentation. Read more here: https://ai.pydantic.dev/output/
-    system_prompt=(
-        "You are an expert football (soccer) scout.",
-        "Based on a user prompt, you should generate a random player.",
-        "It should NOT be a real player, instead come up with a new player that doesn't exist in the real world."
-    )
-)
-
-
-writer_agent = Agent(
-    model="google-gla:gemini-2.5-flash",
-    system_prompt=(
-        "You are a professional and experienced football (soccer) agent.",
-        "With the data provided, you should write a passionate scout report.",
-        "Focus on playstyle, how strengths/weaknesses manifest on the pitch and future potential.",
-        "Do NOT use bullet points. Write in prose (paragraphs)."
-    )
-)
 
 
 async def create_player_file(prompt: str):
@@ -72,7 +53,6 @@ async def create_player_file(prompt: str):
 if __name__ == "__main__":
     seed = random.randint(1, 1000)
     
-
     prompt = f"Generate a player. Random seed: {seed}. Make sure the name is random."
 
     filepath = asyncio.run(create_player_file(prompt))
