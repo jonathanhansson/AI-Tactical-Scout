120c5aa4da7deccefe108092f0aac1b5841a3e6a
jonathanhansson <jonathanhanssondg@gmail.com>
2026-01-22 14:11:05 +0100

    FEATURE: Created a (very dumb) rag_agent and a tool for it to use, called retrieve_first_and_second_pick(). Will expand further.

---
+++ src/backend/rag.py
@@ -0,0 +1,36 @@
+from pydantic_ai import Agent
+from backend.data_models import RagResponse
+from backend.constants import VECTOR_DB_PATH
+
+import lancedb
+
+
+vector_db = lancedb.connect(uri=VECTOR_DB_PATH)
+
+
+rag_agent = Agent(
+    model="google-gla:gemini-2.5-flash",
+    retries=2,
+    system_prompt=(
+        "You are an expert in football (soccer) scouting.",
+        "Always answer based on retrieved knowledge (.txt files), but you CAN mix in your expertise (training data) to make the answer more coherent.",
+        "Don't hallucinate, rather say you can't answer it if the user prompt is outside your knowledge.",
+        "Keep answers clear and concise. Max 6 sentences."
+    ),
+    output_type=RagResponse
+)
+
+
+@rag_agent.tool_plain
+def retrieve_first_and_second_pick(query: str, k=2):
+    # This uses vector search
+
+    results = vector_db["players"].search(query=query).limit(k).to_list()
+    top_result = results[0]
+
+
+    return f"""
+    Player name: {top_result["player_name"]},
+    Filepath: {top_result["filepath"]},
+    Answer: {top_result["scouting_report"]}
+    """
