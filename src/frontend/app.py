0cece7e62493fa709196cc61cd7a9643d6aa0675
jonathanhansson <jonathanhanssondg@gmail.com>
2026-01-27 17:38:19 +0100

    Small frontend requesting from one of our api endpoints

---
+++ src/frontend/app.py
@@ -0,0 +1,15 @@
+import requests
+import streamlit as st
+
+BASE_URL = "http://127.0.0.1:8000"
+SESSION_ID_DEFAULT = "default"
+query = st.text_input("Ask for a player: ")
+
+if st.button("Send question to AI scout") and query.strip() != "":
+    response = requests.post(f"{BASE_URL}/rag/query", json={"query": query, "session_id": SESSION_ID_DEFAULT})
+    data = response.json()
+
+    llm_answer = data.get("answer")
+
+    st.write("Our agent recommends this player")
+    st.write(llm_answer)
