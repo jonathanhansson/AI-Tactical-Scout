bdb325808c8e9c25325372617ea2889a4bda5adb
jonathanhansson <jonathanhanssondg@gmail.com>
2026-01-30 15:29:09 +0100

    I revised constants.py to be more robust

--- src/backend/constants.py
+++ src/backend/constants.py
@@ -1,6 +1,13 @@
 from pathlib import Path
+import os
 
-DATA_PATH = Path(__file__).parents[2] / "data"
+CURRENT_FILE = Path(__file__).resolve()
+
+if os.path.exists("/app"):
+    DATA_PATH = Path("/app/data")
+else:
+    DATA_PATH = CURRENT_FILE.parents[2] / "data"
+
 VECTOR_DB_PATH = Path(__file__).parent / "knowledge_base"
 
 
